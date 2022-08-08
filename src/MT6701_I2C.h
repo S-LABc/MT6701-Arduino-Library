2ff24a3dddde53ef1ae0747abe00dfa3dbeb341d
Roman <romansklyar15@gmail.com>
2022-08-08 21:32:00 +0300

    Update MT6701_I2C.h

--- src/MT6701_I2C.h
+++ src/MT6701_I2C.h
@@ -8,8 +8,6 @@
  ** http://www.magntek.com.cn/upload/MT6701_Rev.1.5.pdf
  * 
  * Контакты:
- ** YouTube - https://www.youtube.com/channel/UCbkE52YKRphgkvQtdwzQbZQ
- ** Telegram - https://www.t.me/slabyt
  ** GitHub - https://github.com/S-LABc
  ** Gmail - romansklyar15@gmail.com
  * 
